7d55c495c127ee339dd40d7fdd3d424bdcee66ed
RobBloxham <Rob.Bloxham209@gmail.com>
2024-06-16 16:47:35 -0500

    displaying titles with images on front end

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+
 import Image from "next/image";
+import { client, urlFor } from "./lib/sanity";
+import { simpleBlogCard } from "./lib/interface";
+import { Card } from "@/components/ui/card";
 
 
 async function getData() {
@@ -7,15 +11,26 @@ async function getData() {
   title,
     smallDescription,
     "currentSlug": slug.current,
-    
+    titleImage,
   }`;
-  
+  const data = await client.fetch(query);
+
+  return data;
 }
 
-export default function Home() {
+export default async function Home() {
+  const data: simpleBlogCard[] = await getData()
+  console.log(data)
   return (
-    <div>
+    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+      {data.map((post, idx) => (
+          <Card key={idx}>
+            <Image src={urlFor(post.titleImage).url()} alt="title image for blog" height={500} width={500} />
+            <h1>{post.title}</h1>
+            <p>{post.smallDescription}</p>
 
+          </Card>
+      ))}
     </div>
   );
 }
