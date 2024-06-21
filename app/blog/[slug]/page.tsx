1e7f39fe38c053907105f12277af30870f9f4317
RobBloxham <Rob.Bloxham209@gmail.com>
2024-06-20 22:39:42 -0500

    again

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import { fullBlog } from "../../lib/interface";
 import { client, urlFor } from "../../lib/sanity"
-import { PortableText } from "next-sanity";
+import { PortableText } from "@portabletext/react";
 import Image from "next/image";
+
+export const revalidate = 30; // revalidate at most 30 seconds
 
 async function getData(slug: string){
     const query = `
@@ -11,18 +13,27 @@ async function getData(slug: string){
     content,
     titleImage
     }[0]
-    `
+    `;
+
     const data = await client.fetch(query);
     return data;
 }
 
-export default async function BlogArticle({params}: {params: {slug: string}}){
+export default async function BlogArticle({
+    params,
+}: {
+    params: {slug: string};
+}) {
     const data: fullBlog = await getData(params.slug);
     return(
         <div className="mt-8">
             <h1>
-                <span className="block text-base text-center text-primary font-semibold tracking-wide uppercase">Robert B - Blog</span>
-                <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">{data.title}</span>
+                <span className="block text-base text-center text-primary font-semibold tracking-wide uppercase">
+                    Robert B - Blog
+                </span>
+                <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">
+                    {data.title}
+                </span>
             </h1>
             <Image 
                 src={urlFor(data.titleImage).url()} 
